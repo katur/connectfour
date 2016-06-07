81a801ca537375a056b4a6930728a3ff4a3d4e5c
Katherine Erickson <katherine.erickson@gmail.com>
2016-06-07 15:21:58 -0400

    Add functions to count consecutive discs and check for win

--- connectfour/model/board.py
+++ connectfour/model/board.py
@@ -1,6 +1,11 @@
 TOP_ROW = 0
 LEFT_COLUMN = 0
 
+HORIZONTAL = (0, 1)
+VERTICAL = (1, 0)
+UP_RIGHT = (1, 1)
+DOWN_RIGHT = (1, -1)
+
 
 class Board(object):
     """
@@ -45,6 +50,10 @@ class Board(object):
         """
         return self.grid[row][column]
 
+    def is_in_bounds(self, row, column):
+        return (row >= self.top_row and row <= self.bottom_row and
+                column >= self.left_column and column <= self.right_column)
+
     def is_column_full(self, column):
         """
         Determine if a column is full of discs.
@@ -62,10 +71,59 @@ class Board(object):
             raise ValueError('Column {} is full'.format(column))
 
         current_row = self.bottom_row
-        current_disc = self.get_disc(current_row, column)
 
-        while current_disc is not None:
+        while self.get_disc(current_row, column) is not None:
             current_row -= 1
-            current_disc = self.get_disc(current_row, column)
 
         self.grid[current_row][column] = disc
+
+    def get_num_outward_discs_1D(self, disc, row, column, step):
+        """
+        Get the number of consecutive disc matches, stepping outward
+        from position [row, column] (exclusive), in a single direction.
+
+        step should be a 2-tuple, (horizontal_step, vertical_step).
+
+        For example, to check upwards, step should be (0, 1).
+        To check diagonally down-left, step should be (-1, -1).
+        """
+        current = (row + step[0], column + step[1])
+        total = 0
+
+        while (self.is_in_bounds(*current) and
+                self.get_disc(*current) == disc):
+            total += 1
+            current = (current[0] + step[0], current[1] + step[1])
+
+        return total
+
+    def get_num_outward_discs_2D(self, disc, row, column, step):
+        """
+        Get the number of consecutive disc matches, stepping outward
+        from position [row, column] (exclusive), in two directions.
+
+        step should be a 2-tuple, (horizontal_step, vertical_step),
+        and the mirror of this step is checked as well.
+
+        For example, to check the horizontal axis, step could be
+        (0, 1) or (0, -1).
+        To check the up-right/down-left diagonal, step could be
+        (1, 1) or (-1, -1).
+        """
+        mirror = (-step[0], -step[1])
+
+        a = self.get_num_outward_discs_1D(disc, row, column, step)
+        b = self.get_num_outward_discs_1D(disc, row, column, mirror)
+
+        return a + b
+
+    def check_for_win(self, disc, row, column, win):
+        """
+        Check for a win if disc were placed at position [row, column].
+        """
+        for step in (HORIZONTAL, VERTICAL, UP_RIGHT, DOWN_RIGHT):
+            x = self.get_num_outward_discs_2D(disc, row, column, step)
+            if x + 1 >= win:
+                return True
+
+        return False
--- example.py
+++ example.py
@@ -9,10 +9,29 @@ print b.get_printable_grid()
 pink = Disc('pink')
 brown = Disc('brown')
 
+b.add_disc(brown, 1)
+
 b.add_disc(pink, 2)
 b.add_disc(brown, 2)
 b.add_disc(pink, 2)
 b.add_disc(brown, 2)
-b.add_disc(pink, 0)
+
+b.add_disc(pink, 3)
+b.add_disc(brown, 3)
+b.add_disc(brown, 3)
+b.add_disc(pink, 3)
+
+b.add_disc(pink, 4)
+b.add_disc(brown, 4)
+b.add_disc(brown, 4)
+b.add_disc(pink, 4)
+
 
 print b.get_printable_grid()
+
+print b.get_num_outward_discs_1D(brown, 2, 2, (0, 1))
+print b.get_num_outward_discs_1D(brown, 2, 2, (0, -1))
+print b.get_num_outward_discs_2D(brown, 2, 2, (0, 1))
+
+print b.check_for_win(brown, 2, 1, 4)
+print b.check_for_win(brown, 3, 5, 5)
