de54a2e0fe263630ce723120d6ca1aeb06720484
Katherine Erickson <katherine.erickson@gmail.com>
2016-06-08 15:59:49 -0400

    A few more super basic unit tests

--- connectfour/model/test_board.py
+++ connectfour/model/test_board.py
@@ -9,7 +9,7 @@ PINK = Disc('pink')
 BROWN = Disc('brown')
 
 
-class EmptyBoardTestCase(unittest.TestCase):
+class TestBoardBasics(unittest.TestCase):
 
     def setUp(self):
         self.board = Board(TEST_NUM_ROWS, TEST_NUM_COLUMNS)
@@ -20,24 +20,37 @@ class EmptyBoardTestCase(unittest.TestCase):
         self.assertEqual(self.board.num_rows, TEST_NUM_ROWS)
         self.assertEqual(self.board.num_columns, TEST_NUM_COLUMNS)
 
+    def test_board_extremes(self):
+        self.assertEqual(self.board.left_column, 0)
+        self.assertEqual(self.board.right_column, TEST_NUM_COLUMNS - 1)
+        self.assertEqual(self.board.top_row, 0)
+        self.assertEqual(self.board.bottom_row, TEST_NUM_ROWS - 1)
+
     def test_grid_dimensions(self):
         self.assertEqual(len(self.board.grid), TEST_NUM_ROWS)
         self.assertEqual(len(self.board.grid[0]), TEST_NUM_COLUMNS)
 
+    def test_grid_size(self):
+        num_positions = 0
+        for i in self.board.grid:
+            for j in i:
+                num_positions += 1
+        self.assertEqual(num_positions, TEST_NUM_ROWS * TEST_NUM_COLUMNS)
+
     def test_add_disc_out_of_bounds_left(self):
         with self.assertRaises(ValueError):
-            self.board.add_disc(PINK, -1)
+            self.board.add_disc(PINK, self.board.left_column - 1)
 
     def test_add_disc_out_of_bounds_right(self):
         with self.assertRaises(ValueError):
-            self.board.add_disc(PINK, self.board.num_columns)
+            self.board.add_disc(PINK, self.board.right_column + 1)
 
     def test_add_and_get_one_disc_left_column(self):
-        self.board.add_disc(PINK, 0)
+        self.board.add_disc(PINK, self.board.left_column)
         self.assertEqual(PINK, self.board.get_disc(self.bottom_left))
         self.assertIsNone(self.board.get_disc(self.bottom_right))
 
     def test_add_and_get_one_disc_right_column(self):
-        self.board.add_disc(PINK, self.board.num_columns - 1)
+        self.board.add_disc(PINK, self.board.right_column)
         self.assertEqual(PINK, self.board.get_disc(self.bottom_right))
         self.assertIsNone(self.board.get_disc(self.bottom_left))
