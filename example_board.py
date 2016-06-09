9792492dc51130259dc4ccb0e7912fc783f7d152
Katherine Erickson <katherine.erickson@gmail.com>
2016-06-08 21:56:28 -0400

    Rename example file

--- example_board.py
+++ example_board.py
@@ -3,31 +3,24 @@ from connectfour.model.disc import Disc
 
 b = Board(num_rows=4, num_columns=6, num_to_win=4)
 
-print b.get_printable_grid()
-
-
 pink = Disc('pink')
 brown = Disc('brown')
 
 b.add_disc(brown, 1)
-
 b.add_disc(pink, 2)
 b.add_disc(brown, 2)
 b.add_disc(pink, 2)
 b.add_disc(brown, 2)
-
 b.add_disc(pink, 3)
 b.add_disc(brown, 3)
+b.add_disc(pink, 0)
 b.add_disc(brown, 3)
+b.add_disc(pink, 4)
+b.add_disc(brown, 4)
 b.add_disc(pink, 3)
-
-b.add_disc(pink, 4)
-b.add_disc(brown, 4)
 b.add_disc(brown, 4)
 b.add_disc(pink, 4)
-
 b.add_disc(brown, 5)
-
 
 print b.get_printable_grid()
 
