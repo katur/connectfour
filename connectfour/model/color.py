7eb61c40d8cb41d3851128e31722c29261555283
Katherine Erickson <katherine.erickson@gmail.com>
2016-06-11 01:38:34 -0400

    Add Color retrieval helper methods

--- connectfour/model/color.py
+++ connectfour/model/color.py
@@ -4,13 +4,26 @@ from enum import Enum
 class Color(Enum):
     """A disc color."""
 
-    black = 1
-    red = 2
-    yellow = 3
-    blue = 4
-    orange = 5
-    green = 6
-    purple = 7
-    pink = 8
-    light_green = 9
-    brown = 10
+    black = 0
+    red = 1
+    yellow = 2
+    blue = 3
+    orange = 4
+    green = 5
+    purple = 6
+    pink = 7
+    light_green = 8
+    brown = 9
+
+
+def get_color_dictionary():
+    colors = {}
+
+    for color in Color:
+        colors[color.value] = color
+
+    return colors
+
+
+def get_color_list():
+    return [color for color in Color]
