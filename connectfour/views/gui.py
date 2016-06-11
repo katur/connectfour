390a0f12db0f12f3d2f31cd2dd4b5c3ecc73c496
Katherine Erickson <katherine.erickson@gmail.com>
2016-06-11 13:31:29 -0400

    Code cleaning

--- connectfour/views/gui.py
+++ connectfour/views/gui.py
@@ -29,6 +29,14 @@ def color_to_tk(color):
     return colors[color]
 
 
+def enable_button(button):
+    button.configure(state=tk.NORMAL)
+
+
+def disable_button(button):
+    button.configure(state=tk.DISABLED)
+
+
 class GUIView(object):
 
     def __init__(self, game):
@@ -41,8 +49,8 @@ class GUIView(object):
 
         window = tk.Tk()
         window.title('Connect Four')
-        self.frame = tk.Frame(window)
-        self.frame.grid()
+        self.main_frame = tk.Frame(window)
+        self.main_frame.grid()
         self.create_setup_frame()
         window.mainloop()
 
@@ -56,7 +64,8 @@ class GUIView(object):
     #######################
 
     def create_setup_frame(self):
-        self.setup_frame = tk.Frame(self.frame, padx=PADDING, pady=PADDING)
+        self.setup_frame = tk.Frame(self.main_frame, padx=PADDING,
+                                    pady=PADDING)
         self.setup_frame.grid()
         self._create_setup_widgets()
 
@@ -81,7 +90,7 @@ class GUIView(object):
         self.add_player_entry.grid(row=row, column=0, pady=PADDING)
 
         add_player_button = tk.Button(self.setup_frame, text='Add Player',
-                                      command=self._add_player)
+                                      command=self.add_player)
         add_player_button.grid(row=row, column=1)
 
     def _create_player_feedback_row(self, row):
@@ -90,12 +99,13 @@ class GUIView(object):
 
     def _create_setup_control_row(self, row):
         self.start_game_button = tk.Button(self.setup_frame, text='Start Game',
-                                           command=self._start_game,
-                                           pady=PADDING, state=tk.DISABLED)
+                                           command=self.start_game,
+                                           pady=PADDING)
         self.start_game_button.grid(row=row, column=0)
+        disable_button(self.start_game_button)
 
         setup_quit = tk.Button(self.setup_frame, text='Quit',
-                               command=self.frame.quit)
+                               command=self.main_frame.quit)
         setup_quit.grid(row=row, column=1)
 
     ######################
@@ -103,7 +113,7 @@ class GUIView(object):
     ######################
 
     def create_game_frame(self):
-        self.game_frame = tk.Frame(self.frame)
+        self.game_frame = tk.Frame(self.main_frame)
         self.game_frame.grid()
         self._create_game_widgets()
 
@@ -125,18 +135,22 @@ class GUIView(object):
         game_title.grid(row=row, columnspan=self.num_columns)
 
     def _create_game_feedback_row(self, row):
-        self.feedback = tk.Label(self.game_frame)
-        self.feedback.grid(row=row, columnspan=self.num_columns)
+        self.game_feedback = tk.Label(self.game_frame)
+        self.game_feedback.grid(row=row, columnspan=self.num_columns)
 
     def _create_column_buttons_row(self, row):
         self.column_buttons = []
         for column in range(self.num_columns):
             button = tk.Button(self.game_frame, text='Play',
-                               command=lambda i=column: self._play_disc(i))
+                               command=lambda i=column: self.play_disc(i))
             button.grid(row=row, column=column)
             self.column_buttons.append(button)
 
     def _create_grid(self, start_row):
+        SLOT_SIZE = 50
+        SLOT_BORDER_WIDTH = 5
+        SLOT_COLOR = 'Yellow'
+
         # Create 2D array to hold pointers to slot widgets
         self.slots = [[None for column in range(self.num_columns)]
                       for row in range(self.num_rows)]
@@ -144,41 +158,55 @@ class GUIView(object):
         for row in range(self.num_rows):
             for column in range(self.num_columns):
                 slot = tk.Frame(self.game_frame,
-                                width=50, height=50,
-                                background='Yellow',
-                                borderwidth=5, relief=tk.RAISED)
+                                width=SLOT_SIZE, height=SLOT_SIZE,
+                                background=SLOT_COLOR,
+                                borderwidth=SLOT_BORDER_WIDTH,
+                                relief=tk.RAISED)
                 slot.grid(row=row + start_row, column=column)
                 self.slots[row][column] = slot
 
     def _create_game_control_row(self, row):
         self.play_again_button = tk.Button(self.game_frame, text='Play Again',
-                                           command=self._play_again)
+                                           command=self.play_again)
         self.play_again_button.grid(row=row, column=0, columnspan=2)
 
-        quit = tk.Button(self.game_frame, text='Quit', command=self.frame.quit)
+        quit = tk.Button(self.game_frame, text='Quit',
+                         command=self.main_frame.quit)
         quit.grid(row=row, column=2, columnspan=2)
+
+    #################################
+    # Button (de)activation helpers #
+    #################################
+
+    def enable_column_buttons(self):
+        for button in self.column_buttons:
+            enable_button(button)
+
+    def disable_column_buttons(self):
+        for button in self.column_buttons:
+            disable_button(button)
 
     ######################
     # Callbacks to model #
     ######################
 
-    def _add_player(self):
+    def add_player(self):
         name = self.add_player_entry.get()
         color = self.colors[0]
         del self.colors[0]
         self.game.add_player(name, color)
 
-    def _start_game(self):
+    def start_game(self):
         self.setup_frame.grid_forget()
         self.create_game_frame()
         self.game.start_round()
 
-    def _play_again(self):
+    def play_again(self):
         self.game_frame.grid_forget()
         self.create_game_frame()
         self.game.start_round()
 
-    def _play_disc(self, column):
+    def play_disc(self, column):
         self.game.play_disc(column)
 
     ################################
@@ -186,26 +214,24 @@ class GUIView(object):
     ################################
 
     def player_added(self, player):
-        # Clear the player name field
-        self.add_player_entry.delete(0, tk.END)
-
         self.player_feedback.configure(text=(
             'Welcome, {}\n'
             'Total players added: {}'
             .format(player, self.game.get_num_players())))
 
         # Only one player is needed to enable start button
-        self.start_game_button.configure(state=tk.NORMAL)
+        enable_button(self.start_game_button)
 
     def round_started(self, round_number):
-        self.play_again_button.configure(state=tk.DISABLED)
+        disable_button(self.play_again_button)
+        self.enable_column_buttons()
 
     def next_player(self, player):
-        self.feedback.configure(text="{}'s turn".format(player))
+        self.game_feedback.configure(text="{}'s turn".format(player))
 
     def try_again(self, player, reason):
-        self.feedback.configure(text="{} try again ({})"
-                                .format(player, reason))
+        self.game_feedback.configure(text='{} try again ({})'
+                                     .format(player, reason))
 
     def disc_played(self, player, position):
         row, column = position
@@ -213,9 +239,11 @@ class GUIView(object):
             background=color_to_tk(player.disc.color))
 
     def round_won(self, player, winning_positions):
-        self.feedback.configure(text="{} won the round".format(player))
-        self.play_again_button.configure(state=tk.NORMAL)
+        self.game_feedback.configure(text='{} won the round'.format(player))
+        self.disable_column_buttons()
+        enable_button(self.play_again_button)
 
     def round_draw(self):
-        self.feedback.configure(text="Round ended in a draw".format())
-        self.play_again_button.configure(state=tk.NORMAL)
+        self.game_feedback.configure(text='Round ended in a draw')
+        self.disable_column_buttons()
+        enable_button(self.play_again_button)
