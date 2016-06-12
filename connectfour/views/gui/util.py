c97a19438f59ed6e1c3f7112e03f62d1b464647c
Katherine Erickson <katherine.erickson@gmail.com>
2016-06-12 17:15:10 -0400

    Ignore exceptions thrown by flash

--- connectfour/views/gui/util.py
+++ connectfour/views/gui/util.py
@@ -1,22 +1,29 @@
 from config import FLASH_CYCLES, FLASH_CYCLE_TIME, FLASH_WAIT_TIME
 
 
-def flash(window, element, color, num_cycles=FLASH_CYCLES):
-    """Make element in window flash.
+def _set_color(element, color):
+    """Set element to color.
+
+    Ignores any exceptions thrown. This is so that elements
+    can be safely destroyed, even while flashing.
+    """
+    try:
+        element.config(bg=color)
+    except Exception:
+        pass
+
+
+def flash(window, element, color, num_cycles=FLASH_CYCLES,
+          cycle_time=FLASH_CYCLE_TIME, wait_time=FLASH_WAIT_TIME):
+    """Make element in window flash a certain color.
 
     Args:
         color: flashes between this color and its original color
-        num_cycles: how many times to flash each color
-
-
-    Flashes between color and its original color.
     """
     original_color = element['bg']
 
     for i in range(num_cycles):
-        window.after(
-            FLASH_WAIT_TIME + FLASH_CYCLE_TIME * i,
-            lambda: element.config(bg=color))
-        window.after(
-            int(FLASH_WAIT_TIME + FLASH_CYCLE_TIME * (i + 0.5)),
-            lambda: element.config(bg=original_color))
+        window.after(wait_time + cycle_time * i,
+                     lambda: _set_color(element, color))
+        window.after(int(wait_time + cycle_time * (i + 0.5)),
+                     lambda: _set_color(element, original_color))
