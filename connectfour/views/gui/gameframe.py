df7620e1079b8e9fc1cbb4fc87876133f4a74156
Katherine Erickson <katherine.erickson@gmail.com>
2016-06-12 15:42:15 -0400

    Group game widgets into separate frames to simplify placement

--- connectfour/views/gui/gameframe.py
+++ connectfour/views/gui/gameframe.py
@@ -32,63 +32,70 @@ class GameFrame(object):
         TITLE_ROW = 0
         FEEDBACK_ROW = 1
         COLUMN_BUTTONS_ROW = 2
-        GRID_START_ROW = 3
-        CONTROL_ROW = GRID_START_ROW + self.num_rows
+        GRID_ROW = 3
+        CONTROL_ROW = 4
 
         self._create_game_title_row(TITLE_ROW)
         self._create_game_feedback_row(FEEDBACK_ROW)
         self._create_column_buttons_row(COLUMN_BUTTONS_ROW)
-        self._create_game_grid(GRID_START_ROW)
+        self._create_game_squares(GRID_ROW)
         self._create_game_control_row(CONTROL_ROW)
 
     def _create_game_title_row(self, row):
         game_title = tk.Label(self.frame, text=GAME_TITLE_TEXT)
-        game_title.grid(row=row, columnspan=self.num_columns)
+        game_title.grid(row=row)
 
     def _create_game_feedback_row(self, row):
         game_feedback = tk.Label(self.frame)
-        game_feedback.grid(row=row, columnspan=self.num_columns)
+        game_feedback.grid(row=row)
         self.widgets['game_feedback'] = game_feedback
 
     def _create_column_buttons_row(self, row):
-        column_buttons = []
+        column_buttons_frame = tk.Frame(self.frame)
+        column_buttons_frame.grid(row=row)
 
+        column_buttons = []
         for column in range(self.num_columns):
             button = tk.Button(
-                self.frame, text=COLUMN_TEXT,
+                column_buttons_frame, text=COLUMN_TEXT,
                 command=lambda i=column: self.view.play_disc(i))
-            button.grid(row=row, column=column)
+            button.grid(row=0, column=column)
             column_buttons.append(button)
 
         self.widgets['column_buttons'] = column_buttons
 
-    def _create_game_grid(self, start_row):
-        num_rows = self.num_rows
-        num_columns = self.num_columns
-        # Create 2D array to hold pointers to slot widgets
-        squares = [[None for column in range(num_columns)]
-                   for row in range(num_rows)]
+    def _create_game_squares(self, outer_row):
+        squares_frame = tk.Frame(self.frame)
+        squares_frame.grid(row=outer_row)
 
-        for row in range(num_rows):
-            for column in range(num_columns):
-                square = tk.Frame(self.frame,
+        # Create 2D array to hold pointers to slot widgets
+        squares = [[None for column in range(self.num_columns)]
+                   for row in range(self.num_rows)]
+
+        for row in range(self.num_rows):
+            for column in range(self.num_columns):
+                square = tk.Frame(squares_frame,
                                   width=SQUARE_SIZE, height=SQUARE_SIZE,
                                   bg=SQUARE_BACKGROUND, bd=SQUARE_BORDER_WIDTH,
                                   relief=tk.RAISED)
-                square.grid(row=row + start_row, column=column)
+                square.grid(row=row, column=column)
                 squares[row][column] = square
 
+        self.widgets['squares_frame'] = squares_frame
         self.widgets['squares'] = squares
 
     def _create_game_control_row(self, row):
-        play_again_button = tk.Button(self.frame, text=PLAY_AGAIN_TEXT,
+        control_frame = tk.Frame(self.frame)
+        control_frame.grid(row=row)
+
+        play_again_button = tk.Button(control_frame, text=PLAY_AGAIN_TEXT,
                                       command=self.view.play_again)
-        play_again_button.grid(row=row, column=0, columnspan=2)
+        play_again_button.grid(row=row, column=0)
         self.widgets['play_again_button'] = play_again_button
 
-        game_quit_button = tk.Button(self.frame, text=QUIT_TEXT,
+        game_quit_button = tk.Button(control_frame, text=QUIT_TEXT,
                                      command=self.view.quit)
-        game_quit_button.grid(row=row, column=2, columnspan=2)
+        game_quit_button.grid(row=row, column=1)
 
     ######################
     # Widget interaction #
