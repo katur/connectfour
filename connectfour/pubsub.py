2997fcd2dcc6adc39b6952ce64d0e12e44d86c8e
Katherine Erickson <katherine.erickson@gmail.com>
2016-06-13 18:45:59 -0400

    Add comments to pubsub

--- connectfour/pubsub.py
+++ connectfour/pubsub.py
@@ -2,6 +2,11 @@ from enum import Enum
 
 
 class Action(Enum):
+    """An action that occurs in the game.
+
+    These are the actions that the model publishes and which the views
+    may subscribe to.
+    """
     player_added = 0
     round_started = 1
     next_player = 2
@@ -11,10 +16,12 @@ class Action(Enum):
     round_draw = 6
 
 
+# Master dictionary to store all subscribed callbacks, keyed on Action
 subscriptions = {}
 
 
 def subscribe(action, callback):
+    """Subscribe so that callback is called when action occurs."""
     if action not in subscriptions:
         subscriptions[action] = []
 
@@ -22,6 +29,11 @@ def subscribe(action, callback):
 
 
 def publish(action, *args, **kwargs):
+    """Publish that an action occurred.
+
+    Any *args and **kwargs are passed along to all subscribed
+    callbacks for this action.
+    """
     if action not in subscriptions:
         return
 
