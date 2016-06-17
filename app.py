cf1b55d3fbe236b4c26270ccd486a74d41bdf1c2
Katherine Erickson <katherine.erickson@gmail.com>
2016-06-17 18:51:05 -0400

    Add logging back to top-level app

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from connectfour.model.model import ConnectFourModel
 from connectfour.views.gui.view import GUIView
-# from connectfour.views.logger.view import LogView
+from connectfour.views.logger.view import LogView
 
 
 model = ConnectFourModel()
 gui_view = GUIView(model)
-# log_view = LogView()
+log_view = LogView()
