40620e3518170c67cfc7ead593aa49236643a324
Katherine Erickson <katherine.erickson@gmail.com>
2016-06-21 13:09:18 -0400

    Move trivial helpers to end of Board class

--- connectfour/model.py
+++ connectfour/model.py
@@ -366,7 +366,7 @@ class Board(object):
 
         Args:
             field_width (Optional[int]): The number of spaces each disc
-                should take up.
+                should take up. If this is not set, a tab is used.
 
         Returns:
             str: A tab- and new-line formatted string of the grid.
@@ -383,64 +383,6 @@ class Board(object):
 
         return output
 
-    def is_row_in_bounds(self, row):
-        """Determine if a row is in bounds.
-
-        Args:
-            row (int): The row to check.
-        Returns:
-            bool: True if in bounds, False otherwise.
-        """
-        return row >= self.top_row and row <= self.bottom_row
-
-    def is_column_in_bounds(self, column):
-        """Determine if a column is in bounds.
-
-        Args:
-            column (int): The column to check.
-        Returns:
-            bool: True if in bounds, False otherwise.
-        """
-        return column >= self.left_column and column <= self.right_column
-
-    def is_in_bounds(self, position):
-        """Determine if position is in bounds.
-
-        Args:
-            position: A 2-tuple in format (row, column).
-        Returns:
-            bool: True if in bounds, False otherwise.
-        """
-        row, column = position
-        return self.is_row_in_bounds(row) and self.is_column_in_bounds(column)
-
-    def is_column_full(self, column):
-        """Determine if a column is full of discs.
-
-        Args:
-            column (int): The column to check.
-        Returns:
-            bool: True if column is full, False otherwise.
-        Raises:
-            ValueError: If column is out of bounds.
-        """
-        if not self.is_column_in_bounds(column):
-            raise ValueError('Column {} is out of bounds'.format(column))
-
-        return self.grid[self.top_row][column] is not None
-
-    def is_full(self):
-        """Determine if this board is entirely full of discs.
-
-        Returns:
-            bool: True if this board is full, False otherwise.
-        """
-        for i in range(self.num_columns):
-            if not self.is_column_full(i):
-                return False
-
-        return True
-
     def reset(self):
         """Reset this board for a new game.
 
@@ -449,24 +391,6 @@ class Board(object):
         for row in range(self.num_rows):
             for column in range(self.num_columns):
                 self.grid[row][column] = None
-
-    def get_disc(self, position):
-        """Retrieve a disc from this board.
-
-        Args:
-            position: A 2-tuple in format (row, column).
-        Returns:
-            Disc: The disc at this position, or None if this
-                position is empty.
-        Raises:
-            ValueError: If position is out of bounds.
-        """
-        if not self.is_in_bounds(position):
-            raise ValueError('Position {} is out of bounds'
-                             .format(position))
-
-        row, column = position
-        return self.grid[row][column]
 
     def add_disc(self, disc, column):
         """Add a disc to a column.
@@ -590,3 +514,83 @@ class Board(object):
             current = tuple(map(operator.add, current, step))
 
         return positions
+
+    ##################
+    # Simple helpers #
+    ##################
+
+    def get_disc(self, position):
+        """Retrieve a disc from this board.
+
+        Args:
+            position: A 2-tuple in format (row, column).
+        Returns:
+            Disc: The disc at this position, or None if this
+                position is empty.
+        Raises:
+            ValueError: If position is out of bounds.
+        """
+        if not self.is_in_bounds(position):
+            raise ValueError('Position {} is out of bounds'
+                             .format(position))
+
+        row, column = position
+        return self.grid[row][column]
+
+    def is_row_in_bounds(self, row):
+        """Determine if a row is in bounds.
+
+        Args:
+            row (int): The row to check.
+        Returns:
+            bool: True if in bounds, False otherwise.
+        """
+        return row >= self.top_row and row <= self.bottom_row
+
+    def is_column_in_bounds(self, column):
+        """Determine if a column is in bounds.
+
+        Args:
+            column (int): The column to check.
+        Returns:
+            bool: True if in bounds, False otherwise.
+        """
+        return column >= self.left_column and column <= self.right_column
+
+    def is_in_bounds(self, position):
+        """Determine if position is in bounds.
+
+        Args:
+            position: A 2-tuple in format (row, column).
+        Returns:
+            bool: True if in bounds, False otherwise.
+        """
+        row, column = position
+        return self.is_row_in_bounds(row) and self.is_column_in_bounds(column)
+
+    def is_column_full(self, column):
+        """Determine if a column is full of discs.
+
+        Args:
+            column (int): The column to check.
+        Returns:
+            bool: True if column is full, False otherwise.
+        Raises:
+            ValueError: If column is out of bounds.
+        """
+        if not self.is_column_in_bounds(column):
+            raise ValueError('Column {} is out of bounds'.format(column))
+
+        return self.grid[self.top_row][column] is not None
+
+    def is_full(self):
+        """Determine if this board is entirely full of discs.
+
+        Returns:
+            bool: True if this board is full, False otherwise.
+        """
+        for i in range(self.num_columns):
+            if not self.is_column_full(i):
+                return False
+
+        return True
