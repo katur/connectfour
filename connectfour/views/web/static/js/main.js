a50137e8992fa83a78256262f3cffa1890430d40
Katherine Erickson <katherine.erickson@gmail.com>
2016-07-26 12:16:33 -0400

    More cleanup of js sending msgs to server

--- connectfour/views/web/static/js/main.js
+++ connectfour/views/web/static/js/main.js
@@ -1,31 +1,29 @@
 $(document).ready(function() {
-	ws = new WebSocket('ws://' + location.hostname + ':' + location.port + location.pathname + '/websocket');
+	window.ws = new WebSocket('ws://' + location.hostname + ':' + location.port +
+                            location.pathname + '/websocket');
 
-	ws.onopen = function() {
-    send_print(ws, "Opened WebSocket");
-    send_create_board(ws, window.num_rows, window.num_columns, window.num_to_win);
-    send_add_player(ws, "Alice", "black");
-    send_add_player(ws, "Bob", "red");
-    send_start_game(ws);
+	window.ws.onopen = function() {
+    sendPrint("Opened WebSocket");
+    setupGame();
 	};
 
-	ws.onmessage = function(e) {
+	window.ws.onmessage = function(e) {
     console.log(e.data);
 	};
 
 });
 
 
-function send_print(ws, message) {
-  ws.send(JSON.stringify({
-    "kind": "print",
-    "message": message,
-  }));
+function setupGame() {
+  sendCreateBoard(window.num_rows, window.num_columns, window.num_to_win);
+  sendAddPlayer("Alice", "black");
+  sendAddPlayer("Bob", "red");
+  sendStartGame();
 }
 
 
-function send_add_player(ws, name, color) {
-  ws.send(JSON.stringify({
+function sendAddPlayer(name, color) {
+  window.ws.send(JSON.stringify({
     "kind": "add_player",
     "name": name,
     "color": color,
@@ -33,8 +31,8 @@ function send_add_player(ws, name, color) {
 }
 
 
-function send_create_board(ws, num_rows, num_columns, num_to_win) {
-  ws.send(JSON.stringify({
+function sendCreateBoard(num_rows, num_columns, num_to_win) {
+  window.ws.send(JSON.stringify({
     "kind": "create_board",
     "num_rows": num_rows,
     "num_columns": num_columns,
@@ -43,16 +41,24 @@ function send_create_board(ws, num_rows, num_columns, num_to_win) {
 }
 
 
-function send_start_game(ws) {
-  ws.send(JSON.stringify({
+function sendStartGame() {
+  window.ws.send(JSON.stringify({
     "kind": "start_game",
   }));
 }
 
 
-function send_play(ws, column) {
-  ws.send(JSON.stringify({
+function sendPlay(column) {
+  window.ws.send(JSON.stringify({
     "kind": "play",
     "column": column,
+  }));
+}
+
+
+function sendPrint(message) {
+  window.ws.send(JSON.stringify({
+    "kind": "print",
+    "message": message,
   }));
 }
