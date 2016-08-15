f20cb031dbe4009835747f57f5261dc0146dd051
Katherine Erickson <katherine.erickson@gmail.com>
2016-08-14 23:31:32 -0400

    Move ws event handlers outside document.ready

--- connectfour/views/web/static/src/js/connectfour.js
+++ connectfour/views/web/static/src/js/connectfour.js
@@ -5,82 +5,83 @@ import { createStore } from "redux";
 import connectfourApp from "./reducers";
 import { setUsername } from "./actions";
 import "../stylesheets/connectfour";
+import $ from "jquery";
 import wsClient from "socket.io-client";
 
-import $ from "jquery";
 
-// Connect to websocket
+/////////////////////////////////////////////
+// Create Redux store and render React app //
+/////////////////////////////////////////////
+
+let store = createStore(connectfourApp);
+// let store = createStore(connectfourApp, window.STATE_FROM_SERVER);
+
+$(document).ready(function() {
+  ReactDOM.render(<App />, document.getElementById("react-root"));
+});
+
+
+//////////////////////////
+// Connect to WebSocket //
+//////////////////////////
+
 const WS_URL = "http://" + document.domain + ":" + location.port;
 window.ws = wsClient(WS_URL);
 
-// Create Redux store
-let store = createStore(connectfourApp);
-// let store = createStore(connectfourApp, window.STATE_FROM_SERVER);
-console.log(store.getState());
 
-// Every time the state changes, log it
-// Note that subscribe() returns a function for unregistering the listener
-let unsubscribe = store.subscribe(function() {
-  console.log(store.getState());
+//////////////////////////////////////////////////////////////////////
+// Respond to WebSocket events (these will mostly update the store) //
+//////////////////////////////////////////////////////////////////////
+
+window.ws.on("message", function(message) {
+  console.log(message);
 });
 
-store.dispatch(setUsername("Jimbo"));
-unsubscribe();
-
-$(document).ready(function() {
-
-  ReactDOM.render(<App />, document.getElementById("react-root"));
-
-  //////////////////////////////
-  // Respond to server events //
-  //////////////////////////////
-
-  window.ws.on("message", function(message) {
-    console.log(message);
-  });
-
-  window.ws.on("board_created", function(data) {
-    updateFeedbackBar("Board created");
-    drawBoard(data.num_rows, data.num_columns, data.num_to_win);
-  });
-
-  window.ws.on("player_added", function(data) {
-    updateFeedbackBar("Welcome, " + data.player);
-    updateGameTitle(data.room);
-  });
-
-  window.ws.on("game_started", function(data) {
-    resetBoard();
-    disablePlayAgainButton();
-    enablePlayButtons();
-    updateGameNumber(data.game_number);
-  });
-
-  window.ws.on("next_player", function(data) {
-    updateFeedbackBar(data.player + "'s turn");
-  });
-
-  window.ws.on("color_played", function(data) {
-    updateGameSquare(data.color, data.position);
-  });
-
-  window.ws.on("try_again", function(data) {
-    updateFeedbackBar(data.player + " try again (" + data.reason + ")");
-  });
-
-  window.ws.on("game_won", function(data) {
-    disablePlayButtons();
-    updateFeedbackBar("Game won by " + data.player);
-    enablePlayAgainButton()
-  });
-
-  window.ws.on("game_draw", function(data) {
-    disablePlayButtons();
-    updateFeedbackBar("Game ended in a draw");
-    enablePlayAgainButton()
-  });
+window.ws.on("board_created", function(data) {
+  updateFeedbackBar("Board created");
+  drawBoard(data.num_rows, data.num_columns, data.num_to_win);
 });
 
+window.ws.on("player_added", function(data) {
+  updateFeedbackBar("Welcome, " + data.player);
+  updateGameTitle(data.room);
+});
+
+window.ws.on("game_started", function(data) {
+  resetBoard();
+  disablePlayAgainButton();
+  enablePlayButtons();
+  updateGameNumber(data.game_number);
+});
+
+window.ws.on("next_player", function(data) {
+  updateFeedbackBar(data.player + "'s turn");
+});
+
+window.ws.on("color_played", function(data) {
+  updateGameSquare(data.color, data.position);
+});
+
+window.ws.on("try_again", function(data) {
+  updateFeedbackBar(data.player + " try again (" + data.reason + ")");
+});
+
+window.ws.on("game_won", function(data) {
+  disablePlayButtons();
+  updateFeedbackBar("Game won by " + data.player);
+  enablePlayAgainButton()
+});
+
+window.ws.on("game_draw", function(data) {
+  disablePlayButtons();
+  updateFeedbackBar("Game ended in a draw");
+  enablePlayAgainButton()
+});
+
+
+///////////////////////////////////////////////////////////////
+// Helpers that i'll mostly be migrating to React components //
+///////////////////////////////////////////////////////////////
 
 function drawBoard(numRows, numColumns, numToWin) {
   var percentage = 80.0 / Math.max(numRows, numColumns);
@@ -110,53 +111,43 @@ function drawBoard(numRows, numColumns, numToWin) {
   resetBoard();
 }
 
-
 function drawPlayAgain() {
   $("#more-controls").append(
     "<button id='play-again' onclick='sendStartGame();'>Play again?</button>");
 }
 
-
 function resetBoard() {
   $(".game-square").css("background", "#fcef03");
 }
 
-
 function updateGameTitle(room) {
   $("#game-title").text("Game Room " + room);
 }
-
 
 function updateGameSquare(color, position) {
   id = "#square-" + position[0] + "-" + position[1];
   $(id).css("background", color);
 }
 
-
 function updateFeedbackBar(message) {
   $("#game-feedback").text(message);
 }
-
 
 function updateGameNumber(number) {
   $("#game-number").text(number);
 }
 
-
 function enablePlayButtons() {
   $(".column-play").removeAttr("disabled");
 }
-
 
 function disablePlayButtons() {
   $(".column-play").attr("disabled", true);
 }
 
-
 function enablePlayAgainButton() {
   $("#play-again").removeAttr("disabled");
 }
-
 
 function disablePlayAgainButton() {
   $("#play-again").attr("disabled", true);
